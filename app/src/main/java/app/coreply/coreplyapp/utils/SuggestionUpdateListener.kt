2dc6e332bdc19aac25c7693973a36f7861dc6eb6
frameartist <seanxionghk@gmail.com>
2025-10-17 16:22:53 +0800

    Remove coreply references

--- app/src/main/java/app/coreply/coreplyapp/utils/SuggestionUpdateListener.kt
+++
@@ -1,7 +0,0 @@
-package app.coreply.coreplyapp.utils
-
-import app.coreply.coreplyapp.network.TypingInfo
-
-interface SuggestionUpdateListener {
-    fun onSuggestionUpdated(typingInfo: TypingInfo,newSuggestion: String)
-}
